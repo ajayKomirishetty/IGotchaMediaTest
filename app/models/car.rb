a0f9a29349ba1ce9bb4502fcde97a2fec693368b
Ajay Komirishetty <Ajay>
2021-07-05 23:13:01 -0400

    added validation to car model

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,6 +1,20 @@
 class Car < ApplicationRecord
     belongs_to :brand
   
-    validates :brand_id, :owner_email, presence: true
+    validates :brand_id,  presence: true
+    validates :model, :doors, :presence =>  { :message => " cannot be blank" }
+    validates_format_of :owner_email, with: URI::MailTo::EMAIL_REGEXP
+    validate :validate_doors
+
+    def validate_doors
+        unless car.doors == 2 || car.doors == 4
+            errors.add(:car, "doors must be 2 or 4" )
+        end
+    end
+
+    def car
+        car = self
+    end
+
   end
   
